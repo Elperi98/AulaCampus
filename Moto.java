e69157745b8602768bb50d9ab8bb21798b30c9a2
tu_nombre_de_usuario <alperi98@hotmail.com>
2024-04-24 14:09:20 +0200

    Update Moto.java

--- Moto.java
+++ Moto.java
@@ -27,3 +27,13 @@ private void frena(int menos) {
             } 
         } 
     } 
+
+public static void main(String[] args) {
+   Moto moto1=new Moto();
+   System.out.println("Velocimetro:");
+   moto1.acelera(160);
+   moto1.acelera(5);
+   moto1.frena(4);
+   moto1.frena(10);
+}
+}
